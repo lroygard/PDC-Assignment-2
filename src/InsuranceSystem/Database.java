e71b6cb3e91351b4ca080d68e6204221877cd6d5
lroygard <lroygard@gmail.com>
2023-05-30 22:56:46 +1200

    Add Database (connection)

---
+++ src/InsuranceSystem/Database.java
@@ -0,0 +1,47 @@
+package InsuranceSystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    
+    private static final String USER_NAME = "pdc";
+    private static final String PASSWORD = "pdc";
+    private static final String URL = "jdbc:derby:Assignment2DB; create=true";
+    
+    Connection conn;
+    
+    public Database() {
+        establishConnection();
+    }
+    
+    public Connection getConnection() {
+        return this.conn;
+    }
+    
+    public void establishConnection() {
+        //Establish a connection to Database
+        try{
+            conn=DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+            System.out.println(URL+" connected...");
+        } catch (SQLException ex) {
+            System.err.println("SQLException: " + ex.getMessage());
+        }
+    }
+    
+    public void closeConnections() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+    
+    /*public static void main(String[] args) {
+        Database db = new Database();
+        System.out.println(db.getConnection());
+    }*/
+}
